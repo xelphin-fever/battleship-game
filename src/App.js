77a9f7aa71b92dcefe2cd59ac958d981a5ea569a
xelphin-fever <xelphin5@gmail.com>
2021-05-30 17:42:16 -0600

    Basic Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
+import Game from './components/Game';
+import ChooseBoard from './components/ChooseBoard';
 import './App.css';
 
 function App() {
+  const shipSizes = [3,2,2];
+  const [playerShipArrays, setPlayerShipArray] = useState([
+    [96,97], [88,89]
+  ]);
+
+  const [gameMode, setGameMode] = useState(false)
+
+  const playerFinishedChoosing = (playerChoices) => {
+    alert('Player Finished Choosing: ', playerChoices);
+    console.log('Player Finished Choosing: ', playerChoices);
+    setPlayerShipArray(playerChoices);
+    setGameMode(true);
+  }
+
+  const displayWinner = (winner) => {
+    if (winner === 0){
+      alert('human has won');
+    } else {
+      alert('computer has won');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-div">
+        <h1>BATTLE-SHIP</h1>
+      </div>
+      <div>
+        {
+          gameMode === false 
+            ? <ChooseBoard shipSizes={shipSizes} finished = {playerFinishedChoosing} />
+            : <Game playerShips ={playerShipArrays} shipSizes={shipSizes} finish={displayWinner}/>
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+  /*
+
+  */
